cbc8f856014c13cf61d4844b9923aaadec3d18c4
Sara Bianchi <sonia_sara@live.it>
2025-09-30 19:15:00 +0200

    fix: keep formatted text when pasting from Office Online

--- src/config/Slate/Link/deserializer.js
+++ src/config/Slate/Link/deserializer.js
@@ -6,10 +6,18 @@ export const simpleLinkDeserializer = (editor, el) => {
   let parent = el;
 
   let children = Array.from(parent.childNodes)
-    .map((el) => deserialize(editor, el))
+    .map((el) => {
+      el.removeAttribute('style');
+      return deserialize(editor, el);
+    })
     .flat();
 
   if (!children.length) children = [{ text: '' }];
+
+  if (el.getAttribute('href') === null) {
+    return jsx('text', {}, children);
+  }
+
   const attrs = {
     type: SIMPLELINK,
     data: {
